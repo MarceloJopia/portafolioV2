5504653c970a65f36422ecec228cedc050e23566
Marcelo Jopia <majopia.v@gmail.com>
2024-06-20 20:19:46 -0400

    VersionExamen1

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -3,17 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const lightboxImage = document.getElementById('lightbox-image');
     const lightboxTitle = document.getElementById('lightbox-title');
     const lightboxDescription = document.getElementById('lightbox-description');
+    const lightboxDate = document.getElementById('lightbox-date');
     const closeBtn = document.querySelector('.lightbox .close');
 
     document.querySelectorAll('.cardProyecto').forEach(card => {
         card.addEventListener('click', () => {
             const imageUrl = card.style.backgroundImage.slice(5, -2);
             const title = card.getAttribute('data-title');
-            const description = card.getAttribute('data-description');
-
-            lightboxImage.src = imageUrl;
+            const description = card.getAttribute('data-description')
+            const date = card.getAttribute('data-date')
+            lightboxImage.style.backgroundImage = `url(${imageUrl})`;
             lightboxTitle.textContent = title;
-            lightboxDescription.textContent = description;
+            lightboxDescription.innerHTML = description;
+            lightboxDate.textContent = date;
 
             lightbox.style.display = 'block';
         });
